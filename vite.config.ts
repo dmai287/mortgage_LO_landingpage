6bcec7dc2b1a41af6e16c08241cd6d1770070c94
Dat Mai <81199025+dmai287@users.noreply.github.com>
2026-04-06 10:19:27 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VERCEL ? '/' : process.env.NODE_ENV === 'production' ? '/mortgage_LO_landingpage/' : '/',
+  base: '/',
 });
